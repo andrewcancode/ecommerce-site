dea5e56eb7ddcc5d468f96edfd681d1e7f733a5f
andrewcancode <alee43207@gmail.com>
2021-10-02 11:35:44 -0400

    commit early, commit often :) ... aka the api fetch works

--- js/app.js
+++ js/app.js
@@ -1,13 +1,35 @@
 console.log('JS connected!');
 
-const $productData = $.ajax({
-    url: "/data/data.json",
-    type: "GET",
-}).then(
-    (data) => {
-        console.log(data);
-},
-    () => {
-        console.log('failed');
+$(() => {
+
+    const render = (data) => {
+        data.forEach(item => {
+            // const $pImage = $('<div class="col-md-3">').html('<img src="' + item.image + '" />');
+            $colDiv = $('<div class="col-md-3">');
+            $pTitle = $('<h3>' + item.title + "</h3>")
+            $pImage = $('<img src="' + item.image + '" />');
+            $pDesc = $('<p>' + item.description + '</p>');
+            $colDiv.append($pTitle);
+            $colDiv.append($pImage)
+            $colDiv.append($pDesc);
+            $('#on-sale-row').append($colDiv);
+        })
     }
-);
+
+    $.ajax({
+        url: "/data/data.json",
+        type: "GET",
+    }).then(
+        (data) => {
+            // console.log(data);
+            render(data);
+    },
+        () => {
+            console.log('failed');
+        }
+    );
+});
+
+// console.log($productData)
+
+// $('.product-top').text($productData[0].price)
